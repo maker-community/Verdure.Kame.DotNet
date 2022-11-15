4517330c37690f833eacde150b6a1c1d0a39f3a0
gil zhang <gil.zhang.dev@outlook.com>
2022-11-15 23:24:32 +0800

    添加项目文件包含舵机对接和显示屏驱动对接。

---
+++ src/Verdure.Kame.Core/Contracts/Services/IQuadrupedFaceScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verdure.Kame.Core
+{
+    public interface IQuadrupedFaceScreen
+    {
+        Task ShowImageAsync(byte[] data);
+
+        void ClearScreen();
+    }
+}
---
+++ src/Verdure.Kame.Core/Services/QuadrupedFaceScreen.cs
@@ -0,0 +1,56 @@
+﻿using Iot.Device.ST7789V3;
+using System.Device.Pwm.Drivers;
+using System.Device.Spi;
+
+namespace Verdure.Kame.Core
+{
+    public class QuadrupedFaceScreen : IQuadrupedFaceScreen, IDisposable
+    {
+        int resetPin = 27;
+        int dataCommandPin = 25;
+        int backlightPin = 18;
+        int blFreq = 1000;
+
+        // SPI0 CS0
+        SpiConnectionSettings senderSettings = new(0, 0)
+        {
+            ClockFrequency = ST7789V3.SpiClockFrequency,
+            Mode = ST7789V3.SpiMode
+        };
+
+        SpiDevice senderDevice;
+
+        SoftwarePwmChannel pwmChannel;
+
+        ST7789V3 lcd;
+
+        public QuadrupedFaceScreen()
+        {
+            senderDevice = SpiDevice.Create(senderSettings);
+
+            pwmChannel = new SoftwarePwmChannel(pinNumber: backlightPin, frequency: blFreq);
+
+            lcd = new ST7789V3(dataCommandPin, senderDevice, resetPin, pwmChannel, shouldDispose: false);
+
+            lcd.Init();
+
+            lcd.SetWindows(0, 0, 172, 320);
+        }
+        public Task ShowImageAsync(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearScreen()
+        {
+            lcd.Clear();
+        }
+
+        public void Dispose()
+        {
+            lcd.Dispose();
+            pwmChannel.Dispose();
+            senderDevice.Dispose();
+        }
+    }
+}
